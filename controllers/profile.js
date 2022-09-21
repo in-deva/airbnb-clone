ef0fd3ac3f8dd29fae8ac54e772ae196e1f94f41
indeva-git <in-deva@hotmail.com>
2022-09-21 23:33:16 +0700

    profile hbs refactoring; setup profile controller get houses find on user id

--- controllers/profile.js
+++ controllers/profile.js
@@ -2,17 +2,30 @@
 const express = require('express')
 const router = express.Router()
 
+const Houses = require('../models/houses')
+
 // Get root
 router.get('/', async (req, res) => {
-  // console.log('profile get route')
+  console.log('profile get route')
   try {
     if (req.isAuthenticated()) {
-      // console.log('authed')
-      console.log(req.user)
+      console.log('authed')
+      // Use the GET /profile to also find in the database all houses that are listed by the user (using the host field),
+      console.log(req.user._id)
+      let houses = await Houses.find({ host: req.user._id })
+      console.log(houses)
+      //.sort(
+      //   req.query.sort ? req.query.sort : 'price'
+      // )
+      //and pass them to the template as the houses array.
+      // In the view, use the houses array to display the data instead of the hardcoded houses.
+      // Make sure that for each house, the "View" button points to /houses/ID_OF_HOUSE and the "Edit" button to /houses/ID_OF_HOUSE/edit (replace ID_OF_HOUSE with the actual _id of the house).
+      // The "List a House" button should point to /houses/create
       res.render('../views/profile', {
         userFull: req.user,
-        user: req.user.name,
-        auth: req.isAuthenticated()
+        user: req.user.name, // needs to be ditched
+        auth: req.isAuthenticated(), //what's this for again?
+        houses
       })
     } else {
       console.log('not logged in')
@@ -26,13 +39,13 @@ router.get('/', async (req, res) => {
 // Patch root
 router.patch('/', async (req, res) => {
   console.log('profile patch route')
-  // Use the PATCH `/profile` controller to
-  // update the user data in the database,
-  //logout user and log back in with new credentials
-  //then redirect to `/profile`
   try {
     if (req.isAuthenticated()) {
       // console.log('authed')
+      // Use the PATCH `/profile` controller to
+      // update the user data in the database,
+      //logout user and log back in with new credentials
+      //then redirect to `/profile`
       res.render('../views/profile')
     } else {
       console.log('not logged in')
