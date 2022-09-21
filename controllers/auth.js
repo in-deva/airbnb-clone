b227b90a793a87f88716698d752bc51972c2f4a4
indeva-git <in-deva@hotmail.com>
2022-09-21 22:20:06 +0700

    auth controller refactoring

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,102 +6,92 @@ const Users = require('../models/users')
 
 // Root
 router.get('/', async (req, res) => {
+  // console.log('auth get route (houses redirect)')
   try {
-    console.log('auth ok')
-    // render
-    // res.render('../views/houses/list')
-    // !!! test this works
     res.redirect('/houses')
   } catch (err) {
+    // !!! properly
     res.redirect('/error')
   }
 })
 
 // Get login
 router.get('/login', async (req, res) => {
+  // console.log('login get route')
   try {
-    console.log('ok its this one')
-    // render
     res.render('../views/login')
   } catch (err) {
+    // !!! properly
     res.redirect('/error')
   }
 })
 
 // Get signup
 router.get('/signup', async (req, res) => {
+  // console.log('signup get route')
   try {
-    console.log('ok')
     // render
     res.render('../views/signup')
   } catch (err) {
+    // !!! properly
     res.redirect('/error')
   }
 })
 
 // Post login
 router.post('/login', async (req, res) => {
+  // console.log('login post route')
   try {
-    // console.log('login okkkkkk')
-    // console.log(req.body)
     // find user with email and password match
     let user = await Users.findOne(req.body)
     if (user) {
-      // console.log('its a match - now log the fuck in')
       // login match
       req.login(user, err => {
         if (err) {
-          throw 'shit'
+          throw 'error' //!!!properly - user already exists, go login, or else...
         }
-        // console.log('woo. what`s it mean now then')
-        // if (req.isAuthenticated()) {
-        //   console.log('authed')
-        // } else {
-        //   console.log('nat authed')
-        // }
         console.log('logged in')
         res.redirect('../houses')
       })
     } else {
-      console.log('uh oh - throw email/pw error here')
+      console.log('uh oh - throw email/pw error here - !!! properly')
     }
   } catch (err) {
+    // !!! properly
     res.redirect('/error')
   }
 })
 
 // Post signup
 router.post('/signup', async (req, res) => {
+  // console.log('signup post route')
   try {
-    console.log('signup post ok')
-    // console.log(req.body)
     let user = await Users.create(req.body)
-    console.log(user)
-    console.log('user created')
-    //run login process like in the login post Router (exact copy, make better)
+    // console.log('user created')
+    //run identical login process
+    // !!! (exact copy, make better)
     user = await Users.findOne(req.body)
     if (user) {
       req.login(user, err => {
         if (err) {
-          throw 'shit'
+          throw 'error' //!!!properly - user already exists, go login, or else...
         }
-        console.log('logged in - do that bit here')
         res.redirect('/houses')
       })
     } else {
-      console.log('uh oh - throw email/pw error here')
+      console.log('throw email/pw error here - !!! properly')
     }
     // redirect to houses route
   } catch (err) {
-    console.log('well at least it failed here')
+    // !!! properly
     res.redirect('/error')
   }
 })
 
 // Get logout
 router.get('/logout', async (req, res) => {
+  // console.log('logout get route')
   try {
-    console.log('logout ok')
     req.logout()
     req.session.destroy(err => {
       if (err) {
@@ -112,6 +102,7 @@ router.get('/logout', async (req, res) => {
       res.redirect('/auth/login')
     })
   } catch (err) {
+    // !!! properly
     res.redirect('/error')
   }
 })
