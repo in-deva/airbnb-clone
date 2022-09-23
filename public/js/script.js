ae41abb5ae21028f0f346a1690ee17df2f8ec506
indeva-git <in-deva@hotmail.com>
2022-09-23 14:25:47 +0700

    house gallery linked to basic function

---
+++ public/js/script.js
@@ -0,0 +1,32 @@
+/*
+// section, we're going to make the gallery of photo
+onclicks dynamic.
+Setup
+Create a script.js file in the /public/js folder of your project and load it into the main.hbs layout.
+*/
+
+/*
+DOM
+Using the DOM and client-size JavaScript, make the large image of the gallery become the same image as the thumbnail that you click on.
+
+Each image in the gallery needs an id
+onclick, then a function
+function changes the image source of the large image
+
+*/
+
+// console.log('hi
+
+function logHi() {
+  console.log('hi')
+}
+
+logHi()
+
+// let productsButtons = document.querySelectorAll('.product > button')
+// let main = document.querySelector('#main')
+//
+// // https://www.w3schools.com/js/js_htmldom_events.asp
+// onclick
+//
+// onclick = logHi
